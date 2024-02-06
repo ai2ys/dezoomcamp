a549b843ba5e909aacade8fe09d877c7fa933a8f
Sylvia Schmitt <ai.two.ys@gmail.com>
2024-02-06 22:40:33 +0100

    updating pipeline

---
+++ homework/02-workflow-orchestration/magic-zoomcamp/data_exporters/gcp_write_partitioned.py
@@ -0,0 +1,30 @@
+from mage_ai.settings.repo import get_repo_path
+from mage_ai.io.config import ConfigFileLoader
+from mage_ai.io.google_cloud_storage import GoogleCloudStorage
+from pandas import DataFrame
+from os import path
+
+import pyarrow as pa
+import pyarrow.parquet as pq
+import os
+
+if 'data_exporter' not in globals():
+    from mage_ai.data_preparation.decorators import data_exporter
+
+
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = '/home/src/<json-key-file>'
+bucket_name = 'mage-zoomcamp-<your-name>-<number>'
+project_id = '<project-id>'
+table_name = 'nyc_taxi_data'
+root_path = f'{bucket_name}/{table_name}'
+
+@data_exporter
+def export_data(data, *args, **kwargs):
+    data['lpep_pickup_date'] = data['lpep_pickup_datetime'].dt.date
+    table = pa.Table.from_pandas(data)
+    gcs = pa.fs.GcsFileSystem()
+    pq.write_to_dataset(
+        table, 
+        root_path=root_path, 
+        partition_cols=['lpep_pickup_date'], 
+        filesystem=gcs)
---
+++ homework/02-workflow-orchestration/magic-zoomcamp/data_loaders/test.sql
@@ -0,0 +1,2 @@
+-- Docs: https://docs.mage.ai/guides/sql-blocks
+SELECT COUNT(1) FROM mage.green_taxi ;
